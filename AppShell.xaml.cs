ecaaa9c6557bd0ba12fdb0c5150c0b1d89c0bfb5
USER <USER@SOFIA>
2023-10-14 22:20:06 +0800

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistrationForm
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistrationForm
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
